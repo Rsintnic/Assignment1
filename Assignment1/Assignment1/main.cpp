76e2387ed43df71f607160c1fe0979bb2d4271d3
Rsintnic <rsintnic@gmail.com>
2018-02-28 15:35:59 -0500

    Added a new struct : sendHelp with function print()

--- Assignment1/Assignment1/main.cpp
+++ Assignment1/Assignment1/main.cpp
@@ -1,4 +1,23 @@
+// this is a test file
+//lets test the conflicts
+
+#include <iostream>
+
+struct sendHelp
+{
+	void print() {
+
+		std::cout << "please\n";
+	}
+	
+};
+
+
 int main() {
 
+	sendHelp s;
+	s.print();
+
+	getchar();
 	return 0;
 }
